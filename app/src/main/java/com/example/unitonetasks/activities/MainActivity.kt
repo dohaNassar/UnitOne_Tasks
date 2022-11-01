482bde40cb7412acc63057e38e16285132a68eae
Doha Nassar <dahoonnassar2017@gmail.com>
2022-11-01 22:45:09 +0200

    Initial commit

--- app/src/main/java/com/example/unitonetasks/activities/MainActivity.kt
+++ app/src/main/java/com/example/unitonetasks/activities/MainActivity.kt
@@ -9,7 +9,7 @@ import com.example.unitonetasks.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
-lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +20,47 @@ lateinit var binding: ActivityMainBinding
         binding.bottomNavigationView.menu.getItem(2).isEnabled = false
 
         swipe(HomeFragment())
+        changeTitle("Home")
+
+        binding.fab.setOnClickListener {
+            swipe(HomeFragment())
+            changeTitle("Home")
+        }
+
+    }
+
+    private fun changeTitle(title: String) {
+        binding.myToolbar.toolbarTitle.text = title
+
     }
 
     fun swipe(fragment: Fragment?) {
-        supportFragmentManager.beginTransaction().replace(R.id.container, fragment!!)
+        supportFragmentManager.beginTransaction().replace(binding.container.id, fragment!!)
             .addToBackStack("").commit()
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        val nav = binding.bottomNavigationView
+        nav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.hotels -> {
+                    changeTitle("Hotels")
+                }
+                R.id.flights -> {
+                    changeTitle("Flights")
+                }
+                R.id.search -> {
+                    changeTitle("Search")
+                }
+                R.id.settings -> {
+                    changeTitle("Settings")
+                }
+
+            }
+            true
+        }
+    }
+
 }
--- app/src/main/java/com/example/unitonetasks/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/unitonetasks/fragments/HomeFragment.kt
@@ -1,16 +1,22 @@
 package com.example.unitonetasks.fragments
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.size
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import androidx.viewpager2.widget.ViewPager2
 import com.example.unitonetasks.adapters.CountryAdapter
 import com.example.unitonetasks.adaptersclass.SliderAdapter
+import com.example.unitonetasks.databinding.ActivityMainBinding
 import com.example.unitonetasks.databinding.FragmentHomeBinding
 import com.example.unitonetasks.interfaces.QuotesApi
 import com.example.unitonetasks.models.AllCity
@@ -18,7 +24,6 @@ import com.example.unitonetasks.models.RetrofitHelper
 import com.example.unitonetasks.models.Slider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 
@@ -26,24 +31,30 @@ class HomeFragment : Fragment() {
 
 
     lateinit var binding: FragmentHomeBinding
-
+    lateinit var activityBinding: ActivityMainBinding
     lateinit var allCities: List<AllCity>
     lateinit var sliders: List<Slider>
+    lateinit var viewPager: ViewPager
+    lateinit var rv: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        activityBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        viewPager = binding.viewPager
+        rv = binding.rv
 
         fetchData()
+
 
         return binding.root
     }
 
 
-
-    private fun fetchData(){
+    private fun fetchData() {
         val quotesApi = RetrofitHelper.getInstance().create(QuotesApi::class.java)
         // launching a new coroutine
         CoroutineScope(Dispatchers.Main).launch {
@@ -52,15 +63,36 @@ class HomeFragment : Fragment() {
             allCities = result.body()!!.data.allCities
             Log.d("doha: ", result.body().toString())
 
-            binding.rv.adapter = CountryAdapter(allCities)
+            rv.adapter = CountryAdapter(allCities)
 
             sliders = result.body()!!.data.slider
 
-            binding.viewPager.adapter = SliderAdapter( sliders)
-
+            viewPager.adapter = SliderAdapter(sliders)
 
 
         }
+    }
+
+    private fun cycledViewPager() {
+
+        viewPager.addOnPageChangeListener(object : OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+        }
+        )
     }
 
 
